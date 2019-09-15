ebd83840dd1c766e50cb7ce7d61c2db386d2301e
Chinmay Chandak <chandakchinmay@gmail.com>
2019-09-15 21:44:51 +0530

    WiP: user profile, XDoC part done some bit

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,5 +1,18 @@
 const User = require('../model/User');
 
 module.exports.profile = function (req, res) {
-  res.render('users/profile')
+  let xdoc = {
+    activities: [],
+    challenges: {
+      active: [],
+      past: []
+    }
+  };
+
+  let projects = {
+    activities: [],
+    participated: [],
+    my: []
+  }
+  res.render('users/profile', { xdoc: xdoc, projects: projects });
 }
