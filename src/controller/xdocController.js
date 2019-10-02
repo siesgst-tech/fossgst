67da7252b5732badd7fca5db5db44a2a3c36c702
Chinmay Chandak <chandakchinmay@gmail.com>
2019-10-02 20:29:18 +0530

    wip: magic, working on checking repo for XDoC

--- src/controller/xdocController.js
+++ src/controller/xdocController.js
@@ -13,10 +13,12 @@ const User = require('../model/User');
 const XDOC = require('../model/Xdoc');
 const Activities = require('../model/XdocActivity');
 
+const sod = moment().startOf('day');
+
 function getActiveXDoCs() {
   const condition = {
     endDate: {
-      $gte: moment().set('hours', '2').subtract(1, 'days')
+      $gte: sod.set('hours', '2').subtract(1, 'days')
     }
   };
   // console.log(condition);
@@ -37,6 +39,43 @@ function getActiveXDoCs() {
 
 function checkXDoC(xdoc) {
   return new Promise((resolve, reject) => {
+    const query = qs.stringify({
+      author: xdoc.userId.ghProfile,
+      since: sod.subtract(1, 'days').toISOString(),
+      until: sod.toISOString(),
+    });
+
+    const options = {
+      hostname: 'api.github.com',
+      port: 443,
+      path: `/repos/${xdoc.userId.ghProfile}/${xdoc.repo}/commits?${query}`,
+      method: 'GET',
+      headers: {
+        'Authorization': `token ${xdoc.userId.ghToken}`,
+        'Accept': 'application/vnd.github.v3+json',
+        'User-Agent': 'FOSSGST'
+      }
+    };
+    // console.log(options);
+
+    const req = https.request(options, res => {
+      // console.log(`status code: ${res.statusCode}`);
+      let data = '';
+      res.on('data', (d) => {
+        data += d;
+      });
+
+      res.on('end', () => {
+        resolve(JSON.parse(data));
+      });
+    });
+
+    req.on('error', (err) => {
+      // console.error(err)
+      reject(err);
+    });
+
+    req.end();
   });
 }
 
