7aa2e9435912c7c4c219630e9f3cfc580c692d40
Chinmay Chandak <chandakchinmay@gmail.com>
2019-10-12 02:28:39 +0530

    WiP: pre release touch-ups, 4
    add new XDoC controller done

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 const User = require('../model/User');
 const XDOC = require('../model/Xdoc');
 
@@ -25,29 +27,59 @@ module.exports.xdocNew = function (req, res) {
 
   // TODO: verify & validate incomming request
 
+  // check if the user has connected his/her GitHub account
+  if (!user.ghProfile) {
+    req.flash('toastMessage', 'Connect your GitHub account first.');
+    req.flash('toastStatus', 'error');
+    res.redirect('/user/profile');
+    return;
+  }
+
   const xdoc = req.body;
-  xdoc.startDate = Date(xdoc.startDate);
+  // console.log(xdoc);
+  const startDate = new moment(xdoc.startDate) || new moment();
+  const endDate = new moment(startDate).add(parseInt(req.body.x), 'days');
+  // console.log(startDate, endDate);
+
+  xdoc.startDate = startDate;
+  xdoc.endDate = endDate;
   xdoc.userId = user._id;
-  xdoc.repo = `https://github.com/${user.ghProfile}/${xdoc.repo}`;
+  xdoc.repo = `${user.ghProfile}/${xdoc.repo}`;
   // console.log(xdoc);
 
   // TODO: chech if XDOC exist with same repo
   // if exists, reject xdoc
 
-  XDOC.create(xdoc).then((doc) => {
-    // console.log(doc);
+  XDOC.findOne({ userId: user._id, endDate: { $gte: moment().toISOString() } }, (err, doc) => {
+    if (err) {
+      req.flash('toastMessage', 'Oops some error.');
+      req.flash('toastStatus', 'error');
+      res.redirect('back');
+    } else if (doc) {
+      req.flash('toastMessage', 'You alread have an active XDoC.');
+      req.flash('toastStatus', 'error');
+      res.redirect('back');
+    } else {
+      XDOC.create(xdoc).then((doc) => {
+        // console.log(doc);
+        req.flash('toastMessage', 'New XDoC Challange added.');
+        req.flash('toastStatus', 'success');
+        res.redirect('/user/profile');
+      }).catch((err) => {
+        // TODO: Handle Error
+        // console.log(err);
 
-    req.flash('toastMessage', 'New XDoC Challange added.');
-    req.flash('toastStatus', 'success');
-    res.redirect('/user/profile');
-  }).catch((err) => {
-    // TODO: Handle Error
-    // console.log(err);
-
-    // Error Code 11000: duplicate key error
-
-    req.flash('toastMessage', 'Oops some error.');
-    req.flash('toastStatus', 'error');
-    res.redirect('back');
+        // Error Code 11000: duplicate key error
+        if (err.code === 11000) {
+          req.flash('toastMessage', 'A XDoC in same repo exists.');
+          req.flash('toastStatus', 'error');
+          res.redirect('back');
+        } else {
+          req.flash('toastMessage', 'Oops some error.');
+          req.flash('toastStatus', 'error');
+          res.redirect('back');
+        }
+      });
+    }
   });
 }
